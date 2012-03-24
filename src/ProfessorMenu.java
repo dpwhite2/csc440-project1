86e7a11afe4abc06027605ee793c6cbb9e22a8c0
Allison Hamann <alhamann@ncsu.edu>
2012-03-24 14:05:11 -0400

    added ProfessorMenu.java

---
+++ src/ProfessorMenu.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+
+/**
+ * @author Allison
+ *
+ */
+public class ProfessorMenu extends Menu{
+
+	private String promptText; //might not need this?
+	private MenuChoice[] menuChoices;
+	
+	/* (non-Javadoc)
+	 * @see Menu#getChoices()
+	 */
+	public MenuChoice[] getChoices() {
+		
+		MenuChoice selectCourse = new MenuChoice("S", "Select Course");
+		MenuChoice addCourse = new MenuChoice("A", "Add Course");
+		MenuChoice back = new MenuChoice("X", "Back");
+		
+		menuChoices[0] = selectCourse;
+		menuChoices[1] = addCourse;
+		menuChoices[2] = back;
+		
+		return menuChoices;
+	}
+
+	/* (non-Javadoc)
+	 * @see Menu#onChoice(MenuChoice)
+	 */
+	public boolean onChoice(MenuChoice choice) {
+		// TODO finish
+		
+		if(choice.equals(menuChoices[0])){
+			//make ProfSelectCourseMenu
+		}
+		else if(choice.equals(menuChoices[1])){
+			//make ProfAddCourseMenu
+		}
+		else if(choice.equals(menuChoices[2])){
+			return false;
+		}
+		else{
+			throw new RuntimeException("ProcessorMenu.onChoice() couldn't match choices");
+		}
+		return false;
+	}
+
+}
